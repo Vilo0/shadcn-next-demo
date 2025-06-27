7055dc0c87bcbbbd606eafc52667fc06e144fcf6
Ed Vilo <thevilos@gmail.com>
2025-06-27 15:48:24 -0400

    chore: add storybook config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/shadcn-next-demo",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
